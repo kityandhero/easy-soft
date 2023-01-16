768a8b651fe6931df97c45730292a75f5d56d136
卢志涛 <kityandhero@126.com>
2023-01-16 21:42:03 +0800

    docs(easy-soft-dva): update

--- packages/easy-soft-utility/src/utils/componentAssist.js
+++ packages/easy-soft-utility/src/utils/componentAssist.js
@@ -57,3 +57,15 @@ export function mergeStyle(target, style) {
 
   return buildStringStyle(target) + buildStringStyle(style);
 }
+
+export function attachPropertiesToComponent(component, properties) {
+  const ret = component;
+
+  for (const key in properties) {
+    if (properties.hasOwnProperty(key)) {
+      ret[key] = properties[key];
+    }
+  }
+
+  return ret;
+}
