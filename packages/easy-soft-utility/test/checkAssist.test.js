e795fc8816186435c64493aff699e50a433adaad
卢志涛 <kityandhero@126.com>
2023-03-27 15:39:21 +0800

    fix(easy-soft-dva): update

--- packages/easy-soft-utility/test/checkAssist.test.js
+++ packages/easy-soft-utility/test/checkAssist.test.js
@@ -1,5 +1,13 @@
-import { checkObjectIsNullOrEmpty } from '../src';
+import { checkObjectIsNullOrEmpty, isNumber } from '../src';
 
 test('checkObjectIsNullOrEmpty -> check {} will be true', () => {
   expect(checkObjectIsNullOrEmpty({})).toBe(true);
+});
+
+test('isNumber -> check 123456 will be true', () => {
+  expect(isNumber(123_456)).toBe(true);
+});
+
+test('isNumber -> check "123456" will be false', () => {
+  expect(isNumber('123456')).toBe(false);
 });
