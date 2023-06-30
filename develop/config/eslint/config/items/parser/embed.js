82d521a66a38d66fc08284c0f6c8a4b56e63304f
卢志涛 <kityandhero@126.com>
2023-06-30 11:17:39 +0800

    fix(easy-soft-dva): update

--- develop/config/eslint/config/items/parser/embed.js
+++ develop/config/eslint/config/items/parser/embed.js
@@ -5,7 +5,15 @@
 const parserJsOptions = {
   requireConfigFile: false,
   babelOptions: {
-    presets: ['@babel/preset-react'],
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic',
+        },
+      ],
+      '@babel/preset-env',
+    ],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
