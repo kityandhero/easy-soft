4ef72934cabfa512c79185d8bb106d7015c1004f
kityandhero <kityandhero@126.com>
2024-03-15 14:28:43 +0800

    fix(easy-soft-dva): :bug: update

--- packages/easy-soft-utility/test/datetime.test.js
+++ packages/easy-soft-utility/test/datetime.test.js
@@ -1,4 +1,10 @@
-import { compareTimeGreaterThan, getTimeStamp, toNumber } from '../src';
+import {
+  compareTimeGreaterThan,
+  createDayJsDatetime,
+  emptyDatetime,
+  getTimeStamp,
+  toNumber,
+} from '../src';
 
 test('compareTimeGreaterThan -> check new greater then 2023-01-01 will be true', () => {
   const tokenDeadline = toNumber(getTimeStamp(new Date()));
@@ -6,4 +12,13 @@ test('compareTimeGreaterThan -> check new greater then 2023-01-01 will be true',
   const result = compareTimeGreaterThan(tokenDeadline, '2023-01-01');
 
   expect(result).toBe(true);
+});
+
+test('createDayJsDatetime -> emptyDatetime will be null', () => {
+  const data = createDayJsDatetime({
+    datetime: emptyDatetime,
+    convertEmptyDatetimeToNull: true,
+  });
+
+  expect(data).toBe(null);
 });
